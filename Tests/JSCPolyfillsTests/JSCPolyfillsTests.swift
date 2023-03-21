c18517c5c07cca6009d639ede12b1cd560471fba
Alexander Steinhauer <alexander@my-e-mail.net>
2023-03-21 16:30:16 +0100

    Initial commit

---
+++ Tests/JSCPolyfillsTests/JSCPolyfillsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import JSCPolyfills
+
+final class JSCPolyfillsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(JSCPolyfills().text, "Hello, World!")
+    }
+}
